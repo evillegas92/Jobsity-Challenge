0437104fb03164da80118905b4449aa14f5fa4d2
Esteban Villegas <evillegas@optimalblue.com>
2019-10-27 22:40:33 -0500

    Only show the last 50 messages

--- StocksChat/StocksChat.Web/wwwroot/js/chat.js
+++ StocksChat/StocksChat.Web/wwwroot/js/chat.js
@@ -8,9 +8,9 @@ document.getElementById("sendButton").disabled = true;
 signalRConnection.on("ReceiveMessage", function (user, message) {
     var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
     var encodedMsg = user + " says: " + msg;
-    var li = document.createElement("li");
-    li.textContent = encodedMsg;
-    document.getElementById("messagesList").appendChild(li);
+
+    insertMessage(encodedMsg);
+
 });
 
 signalRConnection.start().then(function () {
@@ -23,8 +23,22 @@ document.getElementById("sendButton").addEventListener("click", function (event)
     var user = document.getElementById("userInput").innerHTML;
     var message = document.getElementById("messageInput").value;
     document.getElementById("messageInput").value = '';
+
     signalRConnection.invoke("SendMessage", user, message).catch(function (err) {
         return console.error(err.toString());
     });
     event.preventDefault();
 });
+
+function insertMessage(msg)
+{
+    var li = document.createElement("li");
+    li.textContent = msg;
+    var ul = document.getElementById("messagesList");
+  
+    if (ul.getElementsByTagName("li").length >= 50)
+    {
+        ul.removeChild(ul.getElementsByTagName("li")[0]);
+    }
+    ul.appendChild(li);
+}
